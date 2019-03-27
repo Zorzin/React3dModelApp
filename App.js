ea25f9024e6ce7b0933f963a20f665f3ada3a473
Szymon Bakunowicz <zorzin0os@gmail.com>
2019-03-27 21:59:46 +0100

    Accelerometer works

--- App.js
+++ App.js
@@ -1,74 +1,130 @@
 import React, {Component} from 'react';
 import ModelView from 'react-native-gl-model-view';
+import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
 import { Animated, Easing } from 'react-native';
 const AnimatedModelView = Animated.createAnimatedComponent(ModelView);
-
 import {
   setUpdateIntervalForType,
   SensorTypes,
   accelerometer
 } from "react-native-sensors";
 
-
-function accelerometerData() {
-  setUpdateIntervalForType(SensorTypes.accelerometer, 150);
-
-  return accelerometer
-}
+setUpdateIntervalForType(SensorTypes.accelerometer, 100);
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  subscription = null;
 
   constructor(props) {
     super(props);
 
     this.state = {
-      x:new Animated.Value(2),
-      y:0,
+      model:'robot.obj',
+      translateX:new Animated.Value(0),
+      translateY:new Animated.Value(0),
       zoom: new Animated.Value(-2),
+      uiPosition: new Animated.Value(50)
     };
     Object.keys(this.state).forEach(key =>
       this.state[key] instanceof Animated.Value &&
       this.state[key].__makeNative()
     );
-     this.getAccelerometerData();
-  }
-
-  async getAccelerometerData() {
-    const accelerometer = await accelerometerData();
-
-    accelerometer.subscribe(({ x, y, z }) => {
-      console.log(new Animated.Value(x));
-      this.setState({ x:new Animated.Value(x)});
-      this.setState({ y:new Animated.Value(y)});
-    });
   }
 
   render() {
+    const {translateX, translateY} = this.state;
     return (
-      <AnimatedModelView
-        model="robot.obj"
-        texture="bucket.png"
+      <View style={styles.container}>
+        <AnimatedModelView
+          animate
+          model={this.state.model}
+          texture="bucket.png"
 
-        scale={1}
+          scale={0.5}
 
-        rotateZ={180}
-        rotateY={90}
-        animate={true}
-        translateZ={-5}
-        tramslateX={this.state.x*10}
-        tramslateY={this.state.y*10}
-        style={{flex: 1}}
-      />
+          rotateZ={180}
+          rotateY={90}
+          translateZ={-5}
+          translateX={translateX}
+          translateY={translateY}
+          style={{flex: 1}}
+        />
+        <Animated.View style={[styles.buttons]}>
+          {this.renderButton('Bucket', this.openBucket.bind(this))}
+          {this.renderButton('Robot', this.openRobot.bind(this))}
+          {this.renderButton('Shovel', this.openShovel.bind(this))}
+        </Animated.View>
+      </View>
     );
   }
 
-  componentDidMount() {
-    Animated.timing(this.state.zoom, {
-      toValue: -2,
-      useNativeDriver: true,
-      duration: 2000,
-      easing: Easing.bounce
-    }).start();
+  renderButton(label, method) {
+    return (
+      <TouchableOpacity onPress={method}>
+        <Text >{label}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  componentDidMount(): void  {
+    this.subscription = accelerometer.subscribe(({ x, y, z }) => {
+      console.log(x);
+      const {translateX, translateY} = this.state;
+
+      Animated.timing(translateY, {
+        toValue: y/5,
+        useNativeDriver: true,
+        duration: 1
+      }).start();
+
+      Animated.timing(translateX, {
+        toValue: -x,
+        useNativeDriver: true,
+        duration: 1
+      }).start();
+
+
+    });
+  }
+  componentWillUnmount(): void {
+    this.subscription.unsubscribe();
+  }
+
+  openBucket() {
+
+    this.setState({model:'bucket.obj'})
+  }
+
+  openShovel() {
+    this.setState({model:'shovel.obj'})
+  }
+
+  openRobot() {
+    this.setState({model:'robot.obj'})
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent'
+  },
+  view: {
+    flex: 1,
+    backgroundColor: '#ffe2e2'
+  },
+  buttons: {
+    height: 50,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  button: {
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#aaa',
+    borderRadius: 5,
+    textAlign: 'center',
+    fontSize: 12
+  }
+});
